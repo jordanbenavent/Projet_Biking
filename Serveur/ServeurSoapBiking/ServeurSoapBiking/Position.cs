0c4707b0ff0e755a8470822a945f66099ab6eae8
Jordan <jordan.benavent@etu.unice.fr>
2022-11-21 23:24:52 +0100

    récupérer l'itinéraire entre un point A et B (sans parser le json)

---
+++ Serveur/ServeurSoapBiking/ServeurSoapBiking/Position.cs
@@ -0,0 +1,19 @@
+﻿namespace ServeurSoapBiking
+{
+    public class Position
+    {
+        public double[] coordinates { get; set; }
+
+        public string getLongitudeString()
+        {
+            int partEnt = (int)coordinates[0];
+            return partEnt + "." + (coordinates[0] * 10000) % (partEnt * 10000);
+        }
+
+        public string getLattitudeString()
+        {
+            int partEnt = (int)coordinates[1];
+            return partEnt + "." + (coordinates[1] * 10000) % (partEnt * 10000);
+        }
+    }
+}
--- Serveur/ServeurSoapBiking/ServeurSoapBiking/ServiceBiking.cs
+++ Serveur/ServeurSoapBiking/ServeurSoapBiking/ServiceBiking.cs
@@ -6,6 +6,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text.Json;
 using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
 
 namespace ServeurSoapBiking
 {
@@ -31,11 +32,22 @@ namespace ServeurSoapBiking
             }
             return "";
         }
+
+        public Position GetPosition()
+        {
+            foreach (Feature feature in features)
+            {
+                return feature.geometry;
+            }
+            return null;
+        }
     }
 
     public class Feature
     {
         public Properties properties { get; set; }
+
+        public Position geometry { get; set; }
     }
 
     public class Contract
@@ -48,11 +60,6 @@ namespace ServeurSoapBiking
 
     }
 
-    public class Position
-    {
-        public float lat { get; set; }
-        public float lng { get; set; }
-    }
 
     public class Station
     {
@@ -87,27 +94,51 @@ namespace ServeurSoapBiking
 
             public string getRoute(string departure, string arrival)
             {
-                Task<Adress> departAdress = getAdress(departure);
-                //Task<string> arrivalAdress = getAdress(arrival);
-                return departAdress.Result.GetCity();
+            Task<Adress> departAdress = getAdress(departure);
+            Task<Adress> arrivalAdress = getAdress(arrival);
+            Position position = new Position(); position.coordinates = new double[2]; position.coordinates[0] = 8; position.coordinates[1] = 49;
+            Position position2 = new Position(); position2.coordinates = new double[2]; position2.coordinates[0] = 7; position2.coordinates[1] = 49;
+            string result = getRouting(position, position2).Result;
+            return result + "test";
             }
 
             static async Task<Adress> getAdress(string adress)
             {   
                 try
-                {
+                {   
                     HttpResponseMessage responseContract = await Program.client.GetAsync("https://api.openrouteservice.org/geocode/search?" + "api_key=5b3ce3597851110001cf6248560a9124ee2b4b0591d9dcdaf3179440&text=" + adress);
                     Console.WriteLine(responseContract);
                     responseContract.EnsureSuccessStatusCode();
                     string responseBody = await responseContract.Content.ReadAsStringAsync();
                     Adress result = JsonSerializer.Deserialize<Adress>(responseBody);
                     return result;
-            } catch (Exception e)
+                } catch (Exception e)
                 {
 
                 }
                 return null;
             }
+
+            static async Task<string> getRouting(Position start, Position end)
+            {
+            try
+            {
+                
+                HttpResponseMessage responseContract = await Program.client.GetAsync("https://api.openrouteservice.org/v2/directions/foot-walking?" + "api_key=5b3ce3597851110001cf6248560a9124ee2b4b0591d9dcdaf3179440" + "&start=" + start.getLongitudeString() +',' + start.getLattitudeString()+ "&end=" + end.getLongitudeString() +","+ end.getLattitudeString());
+                Console.WriteLine(responseContract);
+                responseContract.EnsureSuccessStatusCode();
+                string responseBody = await responseContract.Content.ReadAsStringAsync();
+                return responseBody;
+                
+            }
+            catch (Exception e)
+            {
+
+            }
+            return "start : " + start.coordinates[0] + " " + start.coordinates[1]; ;
         }
+        }
+
+
 
 }
